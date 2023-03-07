c13b9e32a2d30137fa4cf2795a36dcf7b34ff37f
Philipp Benner <philipp.benner@bam.de>
2023-03-07 16:29:18 +0100

    2023/03/07-16:29:18 (Linux sv2111 unknown)

---
+++ crytures/model_config.py
@@ -0,0 +1,45 @@
+
+from collections import UserDict
+
+## ----------------------------------------------------------------------------
+
+_model_config = {
+    'composition'           : False,
+    'sites'                 : False,
+    'site_features'         : False,
+    'site_features_ces'     : False,
+    'site_features_oxid'    : False,
+    'site_features_csms'    : False,
+    'site_features_ligands' : False,
+    'ligands'               : False,
+    'ce_neighbors'          : False,
+}
+
+## ----------------------------------------------------------------------------
+
+class ModelConfig(dict):
+    def __init__(self, *args,**kwargs):
+        # Set default values
+        for key, value in _model_config.items():
+            self[key] = value
+        super().__init__(self, *args, **kwargs)
+
+    def __setitem__(self, key, value):
+        if key in _model_config:
+            super().__setitem__(key, value)
+        else:
+            raise KeyError(key)
+
+    def __str__(self):
+        result = 'Model config:\n'
+        for key, value in self.items():
+            result += f'-> {key:21}: {value}\n'
+        return result
+
+## ----------------------------------------------------------------------------
+
+DefaultModelConfig = ModelConfig(
+    site_features      = True,
+    site_features_ces  = True,
+    site_features_oxid = True,
+    site_features_csms = True)
