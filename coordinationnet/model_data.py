1518cc6bfadf24405f7ee0ea367768956bd60d48
Philipp Benner <philipp.benner@bam.de>
2023-03-09 10:34:25 +0100

    2023/03/09-10:34:25 (Linux sv2111 unknown)

--- coordinationnet/model_data.py
+++ coordinationnet/model_data.py
@@ -1,10 +1,18 @@
 import torch
 
+from typing import Any
+from pymatgen.core.structure import Structure
+
+from .features_datatypes import CoordinationFeatures
+
 ## ----------------------------------------------------------------------------
 
 class CoordinationFeaturesData(torch.utils.data.Dataset):
 
-    def __init__(self, X, y = None) -> None:
+    def __init__(self, X : list[Any], y = None) -> None:
+
+        if not type(X) == list:
+            raise ValueError(f'X must be of type list, but got type {type(X)}')
 
         if y is None:
             self.y = len(X)*[None]
@@ -16,11 +24,16 @@ class CoordinationFeaturesData(torch.utils.data.Dataset):
 
         self.X = X
 
-        for i, features in enumerate(X):
-            if not features.encoded:
-                self.X[i] = features.encode()
+        for i, item in enumerate(X):
+            if   isinstance(item, Structure):
+                self.X[i] = CoordinationFeatures.from_structure(item, encode=True)
+            elif isinstance(item, CoordinationFeatures):
+                if not item.encoded:
+                    self.X[i] = item.encode()
+            else:
+                raise ValueError(f'Items in X must be of type CoordinationFeatures or Structure, but item {i} is of type {type(item)}')
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.X)
 
     # Called by pytorch DataLoader to collect items that
